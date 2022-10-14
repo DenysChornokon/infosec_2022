fab9f730e9443fd2c99d25bf4f7f5f48e8cfe4f3
Denys Chornokon <denyschornokon3@gmail.com>
2022-10-14 15:54:26 +0300

    +

---
+++ pz1/pz1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class RandomNum
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the seed number-> ");
+        int seed = Convert.ToInt32(Console.ReadLine());
+
+        Random rnd = new Random(seed);
+        for (int r = 0; r < 10; r++)
+        {
+            Console.Write("{0, 3}   ", rnd.Next(0, 15));
+        }
+    }
+}
---
+++ pz2/pz2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class RandomNum
+{
+    public static void Main()
+    {
+
+        for (int r = 0; r < 15; r++)
+        {
+            var rnd = GenerateRandomNumber();
+
+            Console.WriteLine(Convert.ToBase64String(rnd));
+
+        }
+
+    }
+
+
+    public static byte[] GenerateRandomNumber()
+    {
+        using (var randomNumberGenerator = RandomNumberGenerator.Create())
+        {
+            int bit = 64;
+            var randomNumber = new byte[bit];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+}
